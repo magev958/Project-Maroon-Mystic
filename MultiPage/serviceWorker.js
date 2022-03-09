17d6f6c2a5f066fc2ff298ae0a95cab15d2bc3eb
Magnus <15162054+magev958@users.noreply.github.com>
2022-03-09 01:45:34 +0100

    Add files via upload

--- MultiPage/serviceWorker.js
+++ MultiPage/serviceWorker.js
@@ -1,98 +1,68 @@
-/*jshint esversion: 6 */
-
-const PRECACHE = 'precache-v1';
 const OFFLINE_VERSION = 1;
-const OFFLINE_URL = '/404.html';
-const RUNTIME = 'runtime';
-const CACHE_NAME = 'budget01';
-const URLS_CACHE_ONLY = [
-    "404.html"
-];
+const CACHE_NAME = 'offline';
+const OFFLINE_URL = '404.html';
 const PRECACHE_URLS = [
-    "index.html",
 	"favicon.ico",
-	"CascadingStyleSheets/style.css",
-    "JavaScript/jquery-3.6.0.min.js",
-	"JavaScript/function.js",
+    "404.html",
+    "distribution.html",
+    "index.html",
+    "investment.html",
+    "latest.html",
+    "thanks.html",
+    "transactions.html",
+	"manifest.webmanifest",
+	"CascadingStyleSheets/styles.css",
+	"Images/apple-touch-icon.png",
+	"Images/favicon-128x128.png",
+	"Images/icon.svg",
+	"Images/icon-192.png",
+	"Images/icon-512.png",
+    "JavaScript/jQuery-3.6.0.slim.min.js",
 	"Javascript/RGraph/RGraph.common.core.js",
 	"Javascript/RGraph/RGraph.common.sheets.js",
 	"Javascript/RGraph/RGraph.common.key.js",
 	"Javascript/RGraph/RGraph.common.dynamic.js",
 	"Javascript/RGraph/RGraph.common.tooltips.js",
 	"Javascript/RGraph/RGraph.meter.js",
-	"Javascript/RGraph/RGraph.bar.js",
 	"Javascript/RGraph/RGraph.pie.js"
 ];
 
-self.addEventListener("install", function(event) {
-    event.waitUntil(
-        caches.open(PRECACHE)
-			.then(cache => cache.addAll(PRECACHE_URLS))
-			.then(self.skipWaiting());
-        })
-    );
+self.addEventListener('install', (event) => {
+    event.waitUntil((async() => {
+        const cache = await caches.open(CACHE_NAME);
+        await cache.add(new Request(OFFLINE_URL, {
+            cache: 'reload'
+        }));
+    })());
 });
 
-self.addEventListener("fetch", function (event) {
-    const requestURL = new URL(event.request.url);
+self.addEventListener('activate', (event) => {
+    event.waitUntil((async() => {
+        if ('navigationPreload' in self.registration) {
+            await self.registration.navigationPreload.enable();
+        }
+    })());
+    self.clients.claim();
+});
 
-    if (requestURL.pathname === '/') {
-      event.respondWith(getByNetworkFallingBackByCache("46index.html"));
-    } else if (URLS_OVER_NETWORK_WITH_CACHE_FALLBACK.includes(requestURL.href) || URLS_OVER_NETWORK_WITH_CACHE_FALLBACK.includes(requestURL.pathname)) {
-        event.respondWith(getByNetworkFallingBackByCache(event.request));
-    } else if (URLS_CACHE_ONLY.includes(requestURL.href) || URLS_CACHE_ONLY.includes(requestURL.pathname)) {
-        event.respondWith(getByCacheOnly(event.request));
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith((async() => {
+            try {
+                const preloadResponse = await event.preloadResponse;
+                if (preloadResponse) {
+                    return preloadResponse;
+                }
+
+                const networkResponse = await fetch(event.request);
+                return networkResponse;
+            } catch (error) {
+                console.log('Fetch failed; returning offline page instead.', error);
+
+                const cache = await caches.open(CACHE_NAME);
+                const cachedResponse = await cache.match(OFFLINE_URL);
+                return cachedResponse;
+            }
+        })());
     }
 });
-
-self.addEventListener("activate", function (event) {
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                cacheNames.map(function (cacheName) {
-                    if (CACHE_NAME !== cacheName && cacheName.startsWith("budget")) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-/**
- * 1. We fetch the request over the network
- * 2. If successful we add the new response to the cache
- * 3. If failed we return the result from the cache
- *
- * @param request
- * @param showAlert
- * @returns Promise
- */
-const getByNetworkFallingBackByCache = (request, showAlert = false) => {
-    return caches.open(CACHE_NAME).then((cache) => {
-        return fetch(request).then((networkResponse) => {
-            cache.put(request, networkResponse.clone());
-            return networkResponse;
-        }).catch(() => {
-            if (showAlert) {
-                alert('Inget nätverk. Endast lokalt lagrad data.');
-            }
-
-            return caches.match(request);
-        });
-    });
-};
-
-/**
- * Get from cache
- *
- * @param request
- * @returns Promise
- */
-const getByCacheOnly = (request) => {
-    return caches.open(CACHE_NAME).then((cache) => {
-        return cache.match(request).then((response) => {
-            return response;
-        });
-    });
-};
